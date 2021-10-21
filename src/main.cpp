c1a269f60c0cbbc656093cbf98023d601b23a4d7
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-10-21 13:08:10 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 /*
 	Made by @ninjamar (https://github.com/ninjamar)
-	This file runs wnsh
-	If you need the cpp libraries please use wnsh.hpp instead
+	This file runs igsh
+	If you need the cpp libraries please use igsh.hpp instead
 */
 
-#include "wnsh.hpp" // main loop function
+#include "igsh.hpp"
 
 
 int main(){
 	while (true){;
-		wnsh_main(); // run main loop
+		igsh_main(); // run main loop
 	}
-	return 0; // shell returns ok once wnsh_loop is done
+	return 0;
 }
