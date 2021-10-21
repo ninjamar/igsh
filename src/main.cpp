66fd743dad46763070cc402ad91a877aac075a74
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-10-21 09:59:04 +0800

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+	Made by @ninjamar (https://github.com/ninjamar)
+	This file runs wnsh
+	If you need the cpp libraries please use wnsh.hpp instead
+*/
+
+#include "wnsh.hpp" // main loop function
+
+
+int main(){
+	while (true){;
+		wnsh_main(); // run main loop
+	}
+	return 0; // shell returns ok once wnsh_loop is done
+}
