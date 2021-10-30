0b2cd7ea1ef97f3d5bb663ede694f3338eb36fa5
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-10-30 12:57:42 +0800

    comments

--- src/igsh.hpp
+++ src/igsh.hpp
@@ -18,8 +18,10 @@ igsh_main() -> int{
 #include <string>
 #include <sstream>
 #include <vector>
-#include "cmd.hpp"
+#include "cmd.hpp" // all the commands are in cmd.hpp
 
+
+// split string by newlines into a vector
 std::vector<std::string> split_line(std::string s){ 
 	std::string tmp;
 	std::stringstream ss(s);
@@ -29,7 +31,7 @@ std::vector<std::string> split_line(std::string s){
 	}
 	return vwords;
 }	
-
+// read input from cin then split it
 std::vector<std::string> read_cin(){
 	std::string c;
 	std::getline(std::cin,c);
@@ -37,19 +39,22 @@ std::vector<std::string> read_cin(){
 	return sc;
 }
 
+// main function + executes one run of reading input + output
+
 int igsh_main(){
-	typedef int igsh_func(std::vector<std::string>);
-	std::cout << ">>";
-	std::vector<std::string> cmd = read_cin();
+	typedef int igsh_func(std::vector<std::string>); // the function type 
+	std::cout << ">>"; // prompt
+	std::vector<std::string> cmd = read_cin(); // read from cin + split 
 
-	std::string tocall = cmd[0];
-	std::vector<std::string> args = std::vector<std::string>(cmd.begin() + 1, cmd.end());
+	std::string call = cmd[0]; // get the first value in cmd which is the function we are calling
+	
+	std::vector<std::string> args = std::vector<std::string>(cmd.begin() + 1, cmd.end()); // a slice like cmd[1:] so we can get the arguments
 
-	int pos = std::distance(func_names, std::find(func_names, func_names + 3, tocall));
+	int pos = std::distance(func_names, std::find(func_names, func_names + 3, call)); // get the index of the function (call) inside func_names (cmd.hpp)
 	
-	igsh_func* f = (igsh_func*)func[pos];
+	igsh_func* f = (igsh_func*)func[pos]; // create the function from the memory address stored inside func (cmd.hpp)
 	
-	f(args);
+	f(args); // call the function that we made
 	
 	return 0;
 }
